ce98cece8df4988644f872a02de9c84eb5ff5de4
plavib <131249581+plavib@users.noreply.github.com>
2024-03-15 15:53:50 +0530

    Add files via upload

---
+++ multiply7ntimes.c
@@ -0,0 +1,34 @@
+//C program to multiply 7 n times
+/*Name: Pallavi
+  Division:D
+  Roll No : 454
+  USN: 01FE22BEC228
+  */
+#include<stdio.h>
+
+//fn prototype
+int product_of_numbers(int n);
+
+int main()
+{
+    int n,product;
+    printf("Enter how many times 7 should be multiplied\n");
+    scanf("%d",&n);
+    //function call
+    product=product_of_numbers(n);
+
+    return 0;
+}
+
+//fun defition
+int product_of_numbers(int n)
+{
+    int i,product=1;
+    for(i=0;i<n;i++)
+    {
+        product=product*7;
+
+    }
+    printf("Product is %d",product);
+    return product;
+}
