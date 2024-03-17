5eb30d05d6406936d77a9514d2377f5f1edfd007
plavib <131249581+plavib@users.noreply.github.com>
2024-03-17 10:21:02 +0530

    Add files via upload

---
+++ word2.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char name[100];
+    printf("Enter your name:\n");
+    scanf("%s",name);
+    printf("Welcome %s\n",name);
+
+    return 0;
+}
